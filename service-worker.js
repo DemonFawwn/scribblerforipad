d54f036a4cb65ef1526c41046305b385bcbc1ab4
Rin <66930753+DemonFawwn@users.noreply.github.com>
2025-10-08 01:11:54 -0700

    clear

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='scribbler-softweb-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.v3.js','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
